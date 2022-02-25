6bd397959c49aace24e0b4ca7b81f1e4615584c0
liddyjacob <liddyjacob@gmail.com>
2022-02-25 18:26:05 -0500

    Edge hash algorithm seems to work for small block

--- piece/polyomino.cpp
+++ piece/polyomino.cpp
@@ -1,22 +1,151 @@
 #include "polyomino.hpp"
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+#include <algorithm>
 //functions for checking baseform equality
-BaseForm rotateClockWise(const BaseForm& bf, const int& rot){
-    return bf;
+
+
+bool is_rotation(const std::string& str1, const std::string& str2)
+{
+  if(str1.size()!=str2.size())
+    return false;
+
+  std::vector<size_t> prefixes(str1.size(), 0);
+  for(size_t i=1, j=0; i<str1.size(); i++) {
+    while(j>0 && str1[i]!=str1[j])
+      j=prefixes[j-1];
+    if(str1[i]==str1[j]) j++;
+    prefixes[i]=j;
+  }
+
+  size_t i=0, j=0;
+  for(; i<str2.size(); i++) {
+    while(j>0 && str2[i]!=str1[j])
+      j=prefixes[j-1];
+    if(str2[i]==str1[j]) j++;
+  }
+  for(i=0; i<str2.size(); i++) {
+    if(j>=str1.size()) return true;
+    while(j>0 && str2[i]!=str1[j])
+      j=prefixes[j-1];
+    if(str2[i]==str1[j]) j++;
+  }
+
+  return false;
 }
 
-BaseForm translateToOrigin(const BaseForm& bf){
-    return bf;
-}
 
-FreePolyomino::FreePolyomino(BaseForm bf): bf_(bf) {}
+
+FreePolyomino::FreePolyomino(BaseForm bf): bf_(bf) { generateEdgeHash(); }
 
 bool FreePolyomino::operator==(const FreePolyomino& pr){
-    // Check every possible rotation for equivalence of 
-    // this free FreePolyomino
-    for(int i = 0; i < 4; ++i){
-        if (bf_ == translateToOrigin(rotateClockWise(pr.bf_, i))){
-            return true;
-        }
-    }
+    // All we need to do is compare the hash values.
     return false;
+}
+
+void FreePolyomino::generateEdgeHash(){
+    // Initialize to empty.
+    edgeHash_ = std::vector<FreePolyomino::EdgeDir>();
+
+    std::cout << "Generating stuff\n";
+
+    if (this->size() == 0){
+        return;
+    }
+
+    // get any cell in the set
+    Cell edge_start = *(bf_.begin()); 
+    // Note this is not an edge, as it does not 
+    // exist in the matrix
+    
+    // find where the first edge is:
+    while (bf_.count(edge_start)){
+        edge_start.first +=1;
+    }
+
+    // Now we have the first edge, we need to keep this coordinate for when we
+    // finish looping around the polyomino
+    
+    Cell current_edge = edge_start;
+    
+    // need to keep track of the current edge that we are on.
+
+    enum class Direction { UP, RIGHT, DOWN, LEFT };
+    // This map is for utility purposes. If we are going up, then 
+    //
+    // However, if we are on the left side of a block, headed down,
+    // then we need to check for something below
+    std::unordered_map<Direction, std::pair<int,int>> CCW_blockdiff = 
+    {
+        {Direction::UP, std::make_pair(0,1)},
+        {Direction::RIGHT, std::make_pair(1,0)},
+        {Direction::DOWN, std::make_pair(-1,0)},
+        {Direction::LEFT, std::make_pair(0,-1)},
+    };
+
+    std::unordered_map<Direction, std::pair<int,int>> STR_blockdiff = 
+    {
+        {Direction::UP, std::make_pair(-1,1)},
+        {Direction::RIGHT, std::make_pair(1,1)},
+        {Direction::DOWN, std::make_pair(1,-1)},
+        {Direction::LEFT, std::make_pair(-1,-1)},
+    };
+
+    std::unordered_map<Direction, std::pair<int,int>> CW_blockdiff = 
+    {
+        {Direction::UP, std::make_pair(-1,1)},
+        {Direction::RIGHT, std::make_pair(1,1)},
+        {Direction::DOWN, std::make_pair(1,-1)},
+        {Direction::LEFT, std::make_pair(-1,-1)},
+    };
+
+    Direction current_dir = Direction::UP;
+
+    // Note that if you go CCW, you do not move. If you go STRAIGHT, you move in the 
+    // CCW block direction. If you go CW. you move into the STRAIGHT position.
+    // Loop around the entire polymino
+    do {
+        // Check if next turn is right:
+        Cell ccw_blocker = current_edge;
+        auto ccw_diff = CCW_blockdiff[current_dir];
+            ccw_blocker.first += ccw_diff.first; 
+            ccw_blocker.second += ccw_diff.second; 
+
+        if (bf_.count(ccw_blocker)){
+            current_dir = static_cast<Direction>
+                ((static_cast<int>(current_dir) + 1) % 4);
+            edgeHash_.push_back(EdgeDir::CCW);
+
+            continue;
+        }
+
+        // Check if next turn is straight:
+        Cell straight_blocker = current_edge;
+        auto straight_diff = STR_blockdiff[current_dir];
+            straight_blocker.first += straight_diff.first; 
+            straight_blocker.second += straight_diff.second; 
+
+        if (bf_.count(straight_blocker)){
+            current_edge = ccw_blocker; // move to what would have blocked the right.
+            edgeHash_.push_back(EdgeDir::STR);
+
+            continue;
+        }
+ 
+        // If it is nonoe of the others, the turn must be left.
+        
+        current_dir = static_cast<Direction>
+            ((static_cast<int>(current_dir) -1) % 4);
+        current_edge = straight_blocker; 
+        edgeHash_.push_back(EdgeDir::CW);
+    } while (current_edge != edge_start && current_dir != Direction::UP);
+    /// when these two conditions are satysfied we have looped the entire polyomino
+
+    std::for_each(edgeHash_.begin(), edgeHash_.end(),
+        [](auto a){std::cout << static_cast<int>(a) << ", "; });
+    std::cout << std::endl;
+
 }
--- piece/polyomino.hpp
+++ piece/polyomino.hpp
@@ -1,21 +1,69 @@
 #pragma once
 #include <utility>
 #include <vector>
-
+#include <unordered_set>
 /*
 Syntax based off: 
 https://parallelstripes.wordpress.com/2009/12/20/generating-polyominoes/
 */
 using Cell = std::pair<int, int>;
-using BaseForm = std::vector<Cell>;
+
+// Need to define hash for this
+struct pair_hash
+{
+    template <class T1, class T2>
+    std::size_t operator() (const std::pair<T1, T2> &pair) const {
+        return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
+    }
+};
+
+using BaseForm = std::unordered_set<Cell, pair_hash>;
 
 
+/*
+A Free Polyomino without holes
+https://en.wikipedia.org/wiki/Polyomino
+
+If there is a hole, we will have UNDEFINED BEHAVIOR.
+
+Free polyminos
+
+
+Generating polyominos:
+http://www.csun.edu/~ctoth/Handbook/chap14.pdf
+
+Hashing Polyominos:
+https://stackoverflow.com/questions/12010168/algorithm-to-identify-a-unique-free-polyomino-or-polyomino-hash
+
+Compairing circular hashes:
+https://en.wikipedia.org/wiki/Lexicographically_minimal_string_rotation
+https://stackoverflow.com/questions/2553522/interview-question-check-if-one-string-is-a-rotation-of-other-string/2555887#2555887
+
+*/
 class FreePolyomino{
 public:
+    // The base form is needed to compute the edge hash.
     FreePolyomino(BaseForm);
 
     bool operator==(const FreePolyomino&);
     const int size(){return bf_.size();}
 private:
+    // When analyzing multiple polyminos, this is helpful.
+    // STRaight, CounterClockWise, ClockWise.
+    enum class EdgeDir{STR, CCW, CW};
+
+    // This algorithm generates the edgehash and saves it.
+    void generateEdgeHash();
+
+
+
+    // This base form contains the physical structure of the polymino
+    // It is not necessary but will be helpful if we want to add
+    // any display to this program
     BaseForm bf_;
+    // This variable will allow us to quickly build and 
+    // compare polyminos. All we need to do is compare 
+    // edge hashes, which are simply circular lists
+    // describing the edges
+    std::vector<EdgeDir> edgeHash_;
 };
